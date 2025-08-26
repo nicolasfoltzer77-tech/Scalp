77c40a2d3077c1133d44930d80ba7c7723e6e112
nicolasfoltzer77-tech <nicolas.foltzer77@gmail.com>
2025-08-26 14:12:34 +0200

    Update logging_setup.py

--- engine/utils/logging_setup.py
+++ engine/utils/logging_setup.py
@@ -21,12 +21,9 @@ def setup_logger(name: str, log_path: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.handlers.clear()
-
     fh = logging.FileHandler(log_path, encoding="utf-8")
     sh = logging.StreamHandler(sys.stdout)
-
     jf = JsonFormatter()
     fh.setFormatter(jf); sh.setFormatter(jf)
-
     logger.addHandler(fh); logger.addHandler(sh)
     return logger
