f04e7907d800556381b3fe430bf8eca42a44c469
nicolasfoltzer77-tech <nicolas.foltzer77@gmail.com>
2025-08-26 14:07:43 +0200

    Create retry.py

---
+++ engine/utils/retry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+import time
+from typing import Callable, Any, Type, Iterable
+
+def retry(fn: Callable[..., Any],
+          tries: int = 3,
+          delay: float = 1.0,
+          backoff: float = 1.5,
+          exceptions: Iterable[Type[BaseException]] = (Exception,),
+          on_retry: Callable[[int, BaseException], None] | None = None,
+          *args, **kwargs) -> Any:
+    """
+    Exécute fn avec retry/backoff. Renvoie le résultat ou relance la dernière exception.
+    """
+    att = 0
+    cur_delay = delay
+    while True:
+        try:
+            return fn(*args, **kwargs)
+        except exceptions as e:
+            att += 1
+            if att >= tries:
+                raise
+            if on_retry:
+                on_retry(att, e)
+            time.sleep(cur_delay)
+            cur_delay *= backoff
