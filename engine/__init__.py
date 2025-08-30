e4f5c6917a8c6706a196a6e481f2510fc85a0ff4
nicolasfoltzer77-tech <nicolasfoltzer77-tech@users.noreply.github.com>
2025-08-30 09:00:38 +0000

    chore(sync): autosync from server

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,3 +1,4 @@
 # /opt/scalp/engine/__init__.py
 from .utils.pd_compat import patch_pandas_append
 patch_pandas_append()  # active la compat pandas 2.x pour tout l'engine
+# ping Sat Aug 30 09:00:38 AM UTC 2025
